b57eb4a1428fa0fc3978dc28e24d8c047012d173
bunarhaft <irfan.hamzic2003@gmail.com>
2025-10-04 20:36:18 +0200

    weiter gelernt

---
+++ beginner/comment.py
@@ -0,0 +1,10 @@
+#Das ist ein Kommentar
+
+print("Hello World") 
+
+#Das ist ebenso ein Kommentar! :D )"§"§(§($(/$&$&/§$§$))) <- Für Testzwecke 
+
+"""
+Mann kann mehrzeilige Kommentare schreiben mit 3 Gänsefüßchen
+
+"""
---
+++ beginner/variable.py
@@ -0,0 +1,43 @@
+# Variablen werden direkt erstellt wenn man einen Wert zuweist
+
+x = 5
+y = "Mustafa"
+
+print(x)
+print(y)
+
+# Man kann werte sowie Datentypen verändern, der Datentyp passt sich dem Wert an
+
+x = "test"
+
+print(x)
+
+# Casting = Wenn man den Datentyp definieren will macht man das folgendermaßen: 
+
+x = int(3) #beispiel integer
+
+print(x)
+
+# Mit der type() funktion kriegt man den Datentypen raus:
+
+print(type(x))
+
+# Strings könenn mit einem Zitatstrich, oder Gänsefüßchen gemacht werden
+
+y = "hi"
+
+print(y)
+
+y = 'hallo'
+
+print (y)
+
+# Variablen Namen sind Case Sensetive
+
+A = "test"
+
+a = "kleiner Test"
+
+print(a)
+
+print(A)
