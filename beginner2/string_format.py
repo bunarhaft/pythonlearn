7a5631da77bcd94fd0599e1fe04b095373e78c5a
bunarhaft <irfan.hamzic2003@gmail.com>
2025-10-17 00:32:47 +0200

    geschweifte klammern gelernt

---
+++ beginner2/string_format.py
@@ -0,0 +1,12 @@
+#man kann strings und nummern mit F-Strings oder format() methode verbinden
+
+age = 36
+txt = f"ich bin irfan, ich bin {age} jahre alt"
+
+print(txt)
+
+#bei geschweiften klammern hat man einen platzhalter
+
+price = 59
+txt = f"der Preis ist {price} Evro"
+print(txt)
