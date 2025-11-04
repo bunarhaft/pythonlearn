418eb7ff1f12af7a33993a428aacc73039a11c14
bunarhaft <irfan.hamzic2003@gmail.com>
2025-11-04 19:10:03 +0100

    arit

--- operators/arithmetic.py
+++ operators/arithmetic.py
@@ -1,1 +1,22 @@
 #Arithmetische operationen sind Rechnungen plus minus usw
+
+# + Addition
+# - Subtraktion
+# * Multiplikation
+# / Division
+# % Modulu
+# ** exponentiation
+# // floor division
+
+x = 15
+
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
